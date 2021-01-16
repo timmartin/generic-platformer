74b26efbee36c6236720c18659b500dce9ddf958
Tim Martin <tim@asymptotic.co.uk>
2021-01-16 11:24:34 +0000

    Improvements to character movement

--- src/actors/adventurer.ts
+++ src/actors/adventurer.ts
@@ -15,16 +15,17 @@ export class Adventurer extends ex.Actor {
 
   public update(engine: ex.Engine, delta: number) {
     if (engine.input.keyboard.isHeld(ex.Input.Keys.Space)) {
-      this.vel = new ex.Vector(0, -400);
-      this.acc = new ex.Vector(0, 400);
+      this.vel = new ex.Vector(this.vel.x, -400);
+      this.acc = new ex.Vector(this.acc.x, 400);
     }
 
     if (engine.input.keyboard.isHeld(ex.Input.Keys.Left)) {
-      this.acc = new ex.Vector(-800, this.acc.y);
+      this.acc = new ex.Vector(-1000, this.acc.y);
     } else if (engine.input.keyboard.isHeld(ex.Input.Keys.Right)) {
-      this.acc = new ex.Vector(800, this.acc.y);
+      this.acc = new ex.Vector(1000, this.acc.y);
     } else {
       this.acc.x = 0;
+      this.vel.x = 0;
     }
 
     this.vel.x = Math.min(200, this.vel.x);
