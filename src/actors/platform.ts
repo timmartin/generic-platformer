c376001109895f657d98019e70afecc788a68baf
Tim Martin <tim@asymptotic.co.uk>
2021-01-23 11:20:50 +0000

    Draw sprites for the platforms

--- src/actors/platform.ts
+++ src/actors/platform.ts
@@ -1,16 +1,43 @@
 import * as ex from "excalibur";
 
+import {
+  grassLeftSprite,
+  grassMidSprite,
+  grassRightSprite,
+} from "../resources";
+
 export class Platform extends ex.Actor {
   constructor(engine: ex.Engine, x: number, y: number) {
-    const thickness = 20;
+    const width = 430;
+    const thickness = 30;
 
     super({
       x,
       y,
-      width: 400,
+      width: width,
       height: thickness,
-      color: ex.Color.Vermilion,
       collisionType: ex.CollisionType.Fixed,
     });
+  }
+
+  onPostDraw(ctx: CanvasRenderingContext2D) {
+    // Draw the platform a little wider than the actor, because a player
+    // standing on the very extremity shouldn't be supported by the platform.
+    const overhang = 30;
+
+    // Overhang on both ends
+    const drawWidth = this.width + overhang * 2;
+
+    const numTiles = Math.ceil(drawWidth / grassMidSprite.drawWidth);
+
+    grassLeftSprite.draw(ctx, -(drawWidth / 2), -(this.height / 2));
+
+    if (numTiles > 2) {
+      for (let i = 1; i < numTiles - 1; i++) {
+        grassMidSprite.draw(ctx, -(drawWidth / 2) + i * grassMidSprite.drawWidth, -(this.height / 2));
+      }
+    }
+
+    grassRightSprite.draw(ctx, drawWidth / 2 - grassRightSprite.drawWidth, -(this.height / 2));
   }
 }
--- src/resources.ts
+++ src/resources.ts
@@ -1,7 +1,17 @@
 import playerFront from "./assets/player_front.png";
+import grassLeft from "./assets/grass_cliff_left.png";
+import grassMid from "./assets/grass_mid.png";
+import grassRight from "./assets/grass_cliff_right.png";
 
 import * as ex from "excalibur";
 
 export const Resources = {
   playerFront: new ex.Texture(playerFront),
+  grassLeft: new ex.Texture(grassLeft),
+  grassMid: new ex.Texture(grassMid),
+  grassRight: new ex.Texture(grassRight),
 };
+
+export const grassLeftSprite = Resources.grassLeft.asSprite();
+export const grassMidSprite = Resources.grassMid.asSprite();
+export const grassRightSprite = Resources.grassRight.asSprite();
