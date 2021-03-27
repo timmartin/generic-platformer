a5f7aca435049d88d3ea853190dd0a7a6038f69f
Tim Martin <tim@asymptotic.co.uk>
2021-03-27 14:41:09 +0000

    Add resources to start adding a HUD

---
+++ src/actors/score.ts
@@ -0,0 +1,7 @@
+import * as ex from "excalibur";
+
+// Display the score, which is a number of coins collected, as part
+// of the HUD.
+export class Score extends ex.ScreenElement {
+
+}
--- src/resources.ts
+++ src/resources.ts
@@ -4,6 +4,18 @@ import grassMidImage from "./assets/grass_mid.png";
 import grassRightImage from "./assets/grass_cliff_right.png";
 import grassCenterImage from "./assets/grass_center.png";
 import background from "./assets/background.png";
+import coin from "./assets/coin_gold.png";
+import hudCoins from "./assets/hud_coins.png";
+import hud0 from "./assets/hud_0.png";
+import hud1 from "./assets/hud_1.png";
+import hud2 from "./assets/hud_2.png";
+import hud3 from "./assets/hud_3.png";
+import hud4 from "./assets/hud_4.png";
+import hud5 from "./assets/hud_5.png";
+import hud6 from "./assets/hud_6.png";
+import hud7 from "./assets/hud_7.png";
+import hud8 from "./assets/hud_8.png";
+import hud9 from "./assets/hud_9.png";
 
 import * as ex from "excalibur";
 
@@ -14,6 +26,18 @@ export const Resources = {
   grassRight: new ex.Texture(grassRightImage),
   grassCenter: new ex.Texture(grassCenterImage),
   background: new ex.Texture(background),
+  coin: new ex.Texture(coin),
+  hudCoins: new ex.Texture(hudCoins),
+  num0: new ex.Texture(hud0),
+  num1: new ex.Texture(hud1),
+  num2: new ex.Texture(hud2),
+  num3: new ex.Texture(hud3),
+  num4: new ex.Texture(hud4),
+  num5: new ex.Texture(hud5),
+  num6: new ex.Texture(hud6),
+  num7: new ex.Texture(hud7),
+  num8: new ex.Texture(hud8),
+  num9: new ex.Texture(hud9),
 };
 
 export const playerSpritesheet = new ex.SpriteSheet(
